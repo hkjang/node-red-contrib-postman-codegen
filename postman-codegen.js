ac204df0d70ca7f948f881d7c752d31f6be4ea39
hkjang <gagagiga@naver.com>
2021-03-11 10:38:45 +0900

    first commit

---
+++ postman-codegen.js
@@ -0,0 +1,58 @@
+var codegen = require('postman-code-generators');
+var sdk = require('postman-collection');
+module.exports = function (RED) {
+  function postmancodegen(config) {
+    RED.nodes.createNode(this, config);
+    var self = this;
+    this.language = config.language || "";
+    this.variant = config.variant || "";
+    this.api = config.api || "";
+    this.on('input', function(msg) {
+      var language = self.language || msg.language;
+      var variant = self.variant || msg.variant;
+      var api = self.api || msg.api;
+      if(typeof msg.options === 'undefined'){
+        msg.options = {
+          indentCount: 3,
+          indentType: 'Space',
+          trimRequestBody: true,
+          followRedirect: true
+        };
+      }
+      if(api === 'getLanguageList'){
+        msg.payload = codegen.getLanguageList();
+        self.send(msg);
+      }else if(api === 'getOptions'){
+        codegen.getOptions(language, variant, function (error, options) {
+          if (error) {
+            msg.payload = error;
+            self.send(msg);
+          }else{
+            msg.payload = options;
+            self.send(msg);
+          }
+
+        });
+      }else{
+        // var request = new sdk.Request(msg.url);  //using postman sdk to create request
+        var collection = new sdk.Collection(msg.collection);
+        var results = [];
+        collection.items.all().forEach(function (item) {
+          if (item.request !== undefined){
+            codegen.convert(language, variant, item.request, msg.options, function(error, snippet) {
+              if (error) {
+                msg.payload = error;
+                self.send(msg);
+              }else{
+                results.push(snippet);
+              }
+            });
+          }
+        });
+        msg.payload = results;
+        self.send(msg);
+      }
+    });
+  }
+  RED.nodes.registerType('postman-codegen', postmancodegen);
+};
